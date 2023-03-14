2719f2bba9c59e1ffaffef61092fc5a56ec1e798
dok-designr <amr755667@gmail.com>
2023-03-14 17:38:50 +0300

    add lib for get free games, and make basic code to send game name by bot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
 
+const { EpicFreeGames } = require('epic-free-games');
+const epicFreeGames = new EpicFreeGames({ country: 'US', locale: 'us', includeAll: true })
+
+
 // make new bot
 const bot = new Telegraf("6207863211:AAGx9L1MYsJhOxLXoQ8hxV76SL3zI2YgNNU");
 
@@ -10,12 +14,25 @@ bot.start((ctx) => ctx.reply('Welcome'));
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 // , {source: "../1.png"} 
 
-bot.command('gg', ctx =>{
+ epicFreeGames.getGames().then(res => {
+    const games = res.currentGames[0].title;
+
+    bot.command('gg', ctx =>{
+        bot.telegram.sendPhoto(ctx.chat.id,
+            `https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_CalloftheSea_OutoftheBlue_S1_2560x1440-204699c6410deef9c18be0ee392f8335?h=480&quality=medium&resize=1&w=854`,
+             
+            {caption:` #العاب_EpicGames_الأسبوعية 🙂🔥
+
+             • ${games}
+
+            ⭐️ 7.7 | Metacritic`});
+        
+        })
+  }).catch(err => {
+    // Do something
+  });
 
 
-bot.telegram.sendPhoto(ctx.chat.id, {source: "src/Screenshot from 2023-02-28 17-15-30.png"});
-
-})
 
 bot.launch();
 // Enable graceful stopd 
